b8265ffe900735e41fbb7a9721b78ffca911d3c9
耗子也疯狂 <tan.yang86@gmail.com>
2020-04-27 12:43:09 +0800

    current dir bug fix

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,7 +17,6 @@ sed -i '/uci commit system/i uci set system.@system[0].hostname=CrazyWrt'  packa
 # Modify Default Theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-# Update Luci theme argon
-cd package/lean  
-rm -rf luci-theme-argon  
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+# Update Luci theme argon  
+rm -rf package/lean/luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
