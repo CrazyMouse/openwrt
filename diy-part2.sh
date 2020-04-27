8411ca3b9ff7a40836f2301b3dc83d6b7b3949cb
耗子也疯狂 <tan.yang86@gmail.com>
2020-04-27 11:36:21 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,6 +11,9 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.99/g' package/base-files/files/bin/config_generate
 
+# Modify Host Name
+sed -i '/uci commit system/i uci set system.@system[0].hostname=CrazyWrt'  package/lean/default-settings/files/zzz-default-settings
+
 # Update Luci theme argon
 cd package/lean  
 rm -rf luci-theme-argon  
