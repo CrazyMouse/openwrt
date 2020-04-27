8e69ae010e6c9219e000cba37cc7136ad2e596a1
耗子也疯狂 <tan.yang86@gmail.com>
2020-04-27 10:37:39 +0800

    update argon theme to the latest version

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,9 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.99/g' package/base-files/files/bin/config_generate
+
+# Update Luci theme argon
+cd lede/package/lean  
+rm -rf luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
