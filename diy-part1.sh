149cbd22a457ed11c6df1c0fed53d1ccb4b8610c
tanyang <tanyang@jd.com>
2020-04-30 13:18:50 +0800

    实现编译更新后直接可用，不用手动下载clash内核

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,3 +31,10 @@ echo "luci-app-openclash" >> .git/info/sparse-checkout
 git pull origin master
 git branch --set-upstream-to=origin/master master
 cd -
+# integration clash core 实现编译更新后直接可用，不用手动下载clash内核
+curl -sL -m 30 --retry 2 https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz -o /tmp/clash.tar.gz
+tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p package/luci-app-openclash/files/etc/openclash/core
+mv /tmp/clash package/luci-app-openclash/files/etc/openclash/core/clash >/dev/null 2>&1
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
